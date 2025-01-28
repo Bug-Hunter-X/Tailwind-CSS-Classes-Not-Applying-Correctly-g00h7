606fa4f6811104e1318a28be2a00456383a2bd3a
BugHunter-X <askzenai@gmail.com>
2025-01-28 16:42:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+//some tailwind classes are not working
+<div class="bg-red-500 hover:bg-blue-700">
+  <p>This text should have a red background, and turn blue on hover.</p>
+</div>
+```
